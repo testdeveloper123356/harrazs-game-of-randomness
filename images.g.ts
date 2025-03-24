47c853c3d277fa966c8c61bd98ced0b089b27795
testdeveloper123356 <ahmadharraz.iman@gmail.com>
2025-03-25 00:46:10 +0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "playerduck":return img`
+. . . c c c c c c c c b b . . . 
+. . c b d d d d d 5 5 5 b b . . 
+. c d d d d d d 5 5 5 5 5 d b . 
+c b d d d d d 5 5 5 5 5 5 5 b . 
+c d d d c c b 5 5 5 5 5 5 5 b . 
+b d d c d 5 5 b 5 4 4 4 4 4 b . 
+b b d b 5 5 5 d f b 4 4 4 4 4 b 
+. . b d 5 5 b 1 f f 5 4 4 c . . 
+. . b 5 5 b 5 d 1 f 5 d 4 f . . 
+. b d 5 b 5 5 5 5 5 5 5 5 b . . 
+. b b b b b 5 5 5 5 5 5 5 b . . 
+. . . . . b b 5 5 5 5 5 b . . . 
+. . . . . . b b b b b b . . . . 
+. . . . . . . . . b 5 b . . . . 
+. . . . . . . . . . b 5 b . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "tacotacodelicious":return img`
+. . 4 4 e e e . . . . . . . . . 
+. 4 5 4 5 5 4 e . . . . . . . . 
+4 5 4 e 5 5 5 5 e e . . . . . . 
+e 5 e e 5 5 5 5 5 4 e . . . . . 
+e 5 e c 5 4 5 4 5 5 5 e e . . . 
+e 5 c c 5 5 5 5 5 5 5 5 4 e . . 
+e 5 c c e 5 4 5 5 5 4 5 5 5 e . 
+e 5 c e 8 5 5 5 4 5 5 5 5 5 5 4 
+e 4 5 8 6 6 5 5 5 5 5 5 4 5 5 4 
+. e 5 6 7 7 8 5 4 5 4 5 5 5 5 4 
+. e 5 6 2 2 8 8 5 5 5 5 5 4 5 4 
+. . e 5 2 2 7 6 6 4 5 5 5 5 4 . 
+. . . e 5 6 6 7 2 2 6 4 4 4 e . 
+. . . . e 4 5 6 2 2 7 6 6 e . . 
+. . . . . e e 4 5 5 5 e e . . . 
+. . . . . . . e e e e . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
